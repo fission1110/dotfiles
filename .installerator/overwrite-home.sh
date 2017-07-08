3a9cc5c9238fa322279970fcefd3b709ea22410f
fission1110 <rysnxedge1110@gmail.com>
2017-07-08 01:44:36 -0600

    Fix bug where all of /tmp was being copied to ~

--- .installerator/overwrite-home.sh
+++ .installerator/overwrite-home.sh
@@ -7,8 +7,7 @@ fi
 
 cd $INSTALL_DIR
 chown -R $SUDO_USER:$SUDO_USER $INSTALL_DIR
-cp -r $INSTALL_DIR/* ~/
-cp -r $INSTALL_DIR/.* ~/
+cp -r $INSTALL_DIR ~
 
 chown -R $SUDO_USER:$SUDO_USER ~/.byobu/
 chown -R $SUDO_USER:$SUDO_USER ~/.config/terminator
