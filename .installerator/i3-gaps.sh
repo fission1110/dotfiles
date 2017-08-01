b3e00a02e7aa5c24a542d080179cfc385928732a
fission1110 <rysnxedge1110@gmail.com>
2017-07-31 20:12:51 -0600

    autoreconf is in autoconf

--- .installerator/i3-gaps.sh
+++ .installerator/i3-gaps.sh
@@ -11,7 +11,7 @@ read -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	apt-get -y install pkg-config autoreconf automake make clang libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
+	apt-get -y install pkg-config autoconf automake make clang libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
 	add-apt-repository ppa:aguignard/ppa
 	apt-get update
 	apt-get -y install libxcb-xrm-dev
