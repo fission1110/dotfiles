88a48e3aca18b6f6460c7e552744dea66683b57d
Ryan Pierce <ryan.pierce@bluecoat.com>
2017-08-14 16:58:14 -0600

    Fix padawan neovim autoload directory.

--- .installerator/nvim-padawan.sh
+++ .installerator/nvim-padawan.sh
@@ -39,9 +39,10 @@ git clone --recursive https://github.com/padawan-php/deoplete-padawan.git
 
 rm -r $INSTALL_DIR/.config/nvim/bundle/deoplete.nvim/rplugin/python3/deoplete/sources/deoplete_padawan 2>/dev/null
 rm -r $INSTALL_DIR/.config/nvim/bundle/deoplete.nvim/rplugin/python3/deoplete/sources/deoplete_padawan.py 2>/dev/null
-rm -r $INSTALL_DIR/.config/nvim/bundle/deoplete.vim/autoload/padawan.vim
+rm -r $INSTALL_DIR/.config/nvim/bundle/deoplete.vim/autoload/deoplete/sources/padawan.vim
+mkdir -p $INSTALL_DIR/.config/nvim/bundle/deoplete.vim/autoload/deoplete/sources/
 
 cp -r ./deoplete-padawan/rplugin/python3/deoplete/sources/deoplete_padawan $INSTALL_DIR/.config/nvim/bundle/deoplete.nvim/rplugin/python3/deoplete/sources/
 cp -r ./deoplete-padawan/rplugin/python3/deoplete/sources/deoplete_padawan.py $INSTALL_DIR/.config/nvim/bundle/deoplete.nvim/rplugin/python3/deoplete/sources/
 
-cp -r ./deoplete-padawan/autoload/deoplete/sources/padawan.vim $INSTALL_DIR/.config/nvim/bundle/deoplete.vim/autoload/
+cp -r ./deoplete-padawan/autoload/deoplete/sources/padawan.vim $INSTALL_DIR/.config/nvim/bundle/deoplete.vim/autoload/deoplete/sources/
